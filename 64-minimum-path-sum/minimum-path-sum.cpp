edbb902f577cc8e41fda9c56e3f19e9d8273ebbf
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2024-02-02 17:09:28 +0530

    Time: 11 ms (28.15%) | Memory: 12.4 MB (16.64%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
 
-    int call(int i, int j, int n, int m, vector<vector<int>>&dp, vector<vector<int>> &grid){
-        if(i >= n or j >= m)return INT_MAX;
-        if(i==n-1 and j==m-1)return grid[i][j];
-        if(dp[i][j]!=INT_MAX)return dp[i][j];
-        return dp[i][j] = min(call(i+1,j,n,m,dp,grid),call(i,j+1,n,m,dp,grid))+grid[i][j];
+    int call(int n, int m, vector<vector<int>>&dp, vector<vector<int>> &grid){
+        if(n < 0 or m < 0)return INT_MAX;
+        if(n==0 and m==0)return grid[n][m];
+        if(dp[n][m]!=INT_MAX)return dp[n][m];
+        return dp[n][m] = min(call(n-1,m,dp,grid),call(n,m-1,dp,grid))+grid[n][m];
     }
 
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<int>>dp(n,vector<int>(m,INT_MAX));
-        return call(0,0,n,m,dp,grid);
+        return call(n-1,m-1,dp,grid);
     }
 };
