02f8c75237952b4e8dce5eb5f1438396de7a1744
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2024-02-10 17:33:50 +0530

    Time: 5 ms (64.45%) | Memory: 15.3 MB (33.37%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
+
+    int count_pal(int l, int r, string s){
+        int res = 0;
+        while(l >= 0 and r < s.size() and s[l] == s[r]){
+            res++;
+            l--;
+            r++;
+        }
+        return res;
+    }
+
     int countSubstrings(string s) {
         int ans = 0;
         for(int i = 0 ; i < s.size() ; i++){
             int l = i, r = i;
-            while(l >= 0 and r < s.size() and s[l] == s[r]){
-                ans++;
-                l--;
-                r++;
-            }
+            ans += count_pal(l,r,s);
             l = i;
             r = i+1;
-            while(l >= 0 and r < s.size() and s[l] == s[r]){
-                ans++;
-                l--;
-                r++;
-            }
+            ans += count_pal(l,r,s);
         }
         return ans;
     }
