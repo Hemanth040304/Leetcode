f2a6e65ba6150c861daf2618de2138ba7981f8e7
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2024-02-17 02:32:08 +0530

    Time: 41 ms (37.40%) | Memory: 15.7 MB (66.71%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+    int Return_Maximal(vector<int> &ds){
+        int n = ds.size();
+        int maxi = 0;
+        for (int i = 0; i < n; i++){
+            int mini = INT_MAX;
+            int len = 0;
+            for (int j = i; j < n; j++){
+                mini = min(mini, ds[j]);
+                len = j - i + 1;
+                maxi = max(maxi, len * mini);
+            }
+        }
+        return maxi;
+    }
+
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int ans = 0;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int>streatch(m,0);
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                if(matrix[i][j] == '0')streatch[j] = 0;
+                else streatch[j] += matrix[i][j] - '0';
+            }
+            ans = max(ans, Return_Maximal(streatch));
+        }
+        return ans;
+    }
+};
