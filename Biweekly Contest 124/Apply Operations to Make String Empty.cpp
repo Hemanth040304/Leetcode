bbc942e59658fd537ada4f93e26271364a3c28b3
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2024-02-17 21:48:05 +0530

    Biweekly Contest 124

---
+++ Biweekly Contest 124/Apply Operations to Make String Empty.cpp
@@ -0,0 +1,20 @@
+//Apply Operations to Make String Empty
+
+class Solution {
+public:
+    string lastNonEmptyString(string s) {
+        int maxi = 0;
+        string ans;
+        unordered_map<char,int>mp;
+        for(auto it: s){
+            mp[it]++;
+            if(mp[it] > maxi){
+                maxi = mp[it];
+                ans = "";
+                ans += it;
+            }
+            else if(mp[it] == maxi)ans += it;
+        }
+        return ans;
+    }
+};
