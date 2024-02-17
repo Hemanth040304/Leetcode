703af2f1b4c12ee0e07bf4f06413ce9f27d0d163
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2024-02-17 23:32:40 +0530

    Time: 633 ms (53.63%) | Memory: 11.7 MB (20.33%) - LeetSync

---
+++ 79-word-search/word-search.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    bool Search(int i, int j, int n, int m, vector<vector<char>>& board, string& word, int ind, int len, vector<vector<bool>>& visited) {
+    if (ind == len)
+        return true;
+    
+    if (i < 0 || i >= n || j < 0 || j >= m || visited[i][j] || board[i][j] != word[ind])
+        return false;
+    
+    visited[i][j] = true;
+    
+    if (Search(i - 1, j, n, m, board, word, ind + 1, len, visited) ||
+        Search(i + 1, j, n, m, board, word, ind + 1, len, visited) ||
+        Search(i, j - 1, n, m, board, word, ind + 1, len, visited) ||
+        Search(i, j + 1, n, m, board, word, ind + 1, len, visited))
+        return true;
+    
+    visited[i][j] = false;
+    
+    return false;
+}
+
+
+    bool exist(vector<vector<char>>& board, string word) {
+        int n = board.size();
+        int m = board[0].size();
+        vector<vector<bool>> visited(n,vector<bool>(m,0));
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                if(Search(i,j,n,m,board,word,0,word.size(),visited))return true;
+            }
+        }
+        return false;
+    }
+};
