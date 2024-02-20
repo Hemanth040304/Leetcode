09f0dd3226139096eabdb00d4f93ba71b1f67824
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2024-02-20 18:01:35 +0530

    Time: 12 ms (49.01%) | Memory: 16.2 MB (13.69%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,24 +3,25 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<int>> vis(n,(vector<int>(m,0)));
-        for(int i = 0 ; i < n ; i++) {
+        vector<int>row,col;
+        for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
-                if(matrix[i][j] == 0 and (!vis[i][j])){
-                    vis[i][j]=1;
-                    for(int x = 0 ; x < n ; x++){
-                        if(matrix[x][j] != 0){
-                            matrix[x][j] = 0;
-                            vis[x][j] = 1;
-                        }
-                    }
-                    for(int x = 0; x < m ; x++){
-                        if(matrix[i][x] != 0){
-                            matrix[i][x] = 0;
-                            vis[i][x] = 1;
-                        }
-                    }
+                if(matrix[i][j] == 0){
+                    row.emplace_back(i);
+                    col.emplace_back(j);
                 }
+            }
+        }
+        for(int i = 0 ; i < row.size() ; i++){
+            int pos = row[i];
+            for(int j = 0 ; j < m ; j++){
+                matrix[pos][j] = 0;
+            }
+        }
+        for(int i = 0 ; i < col.size() ; i++){
+            int pos = col[i];
+            for(int j = 0 ; j < n ; j++){
+                matrix[j][pos] = 0;
             }
         }
     }
