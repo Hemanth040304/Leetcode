6c74081d7268b18cd3649ca0c4c2e5c4af8808de
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2024-03-07 22:20:58 +0530

    Time: 4 ms (32.95%) | Memory: 7.9 MB (62.91%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         vector<vector<int>> ans;
 
         for (int i = 0 ; i < numRows; i++){
