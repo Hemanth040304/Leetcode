f7f9a71e5d738deee5cddc86b6ed80c5a4c451a7
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2024-03-08 14:47:54 +0530

    Time: 3 ms (84.14%) | Memory: 22.1 MB (13.73%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int maxFrequencyElements(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         int numberOfNumbers = 0;
         map<int,int> mp;
         int maxi = 0;
