41558e665c33ddb29182eacab94a3c9e2049dee5
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2024-07-17 23:44:31 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (8.12%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -11,18 +11,10 @@
  */
 class Solution {
 public:
-    bool fun(TreeNode *r1,TreeNode *r2,bool x){
-        if((r1==NULL and r2!=NULL)or(r1!=NULL and r2==NULL)){
-            x = false;
-            return x;
-        }
-        if(r1==NULL and r2==NULL)return x;
-        if(r1->val==r2->val)x=true;
-        else x = false;
-        return fun(r1->left,r2->left,x) && fun(r1->right,r2->right,x);
-    }
-    bool isSameTree(TreeNode* r1, TreeNode* r2) {
-        if(r1==NULL and r2==NULL)return true;
-        return fun(r1,r2,false);
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p or !q) return p == q;
+        return (p -> val == q -> val)
+         and (isSameTree(p -> left, q -> left))
+         and (isSameTree(p -> right, q -> right));
     }
 };
