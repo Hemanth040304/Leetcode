5cca1e8a36c336ce4976694e28fbe0e8140313c1
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2024-11-02 13:07:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (59.78%) - LeetSync

--- 2580-circular-sentence/circular-sentence.cpp
+++ 2580-circular-sentence/circular-sentence.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     bool isCircularSentence(string sentence) {
-        int c = 0, n = sentence.size();
+        int n = sentence.size();
         for (int i = 0; i < n; i++) {
-            if (sentence[i] == ' ') c++;
             if (sentence[i] == ' ' and sentence[i - 1] != sentence[i + 1]) return false;
         }
         
