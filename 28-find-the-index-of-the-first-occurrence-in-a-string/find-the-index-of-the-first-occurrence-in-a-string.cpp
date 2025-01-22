57beaa629915385966999c52c24cc1b64ce95da2
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2025-01-22 22:35:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (78.17%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,8 +1,13 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        if(haystack.size() < needle.size()) return -1;
-        if((haystack).find(needle) != string::npos) return (haystack).find(needle);
-      return -1;  
+        if (haystack.size() < needle.size()) return -1;
+        for (int i = 0; i < haystack.size(); i++) {
+            if (haystack[i] == needle[0]) {
+                string compare = haystack.substr(i, needle.size());
+                if (compare == needle) return i;
+            }
+        }
+        return -1;
     }
 };
