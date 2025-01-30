f4f77fc0ee5eb33cd762c1a291c3e1c36916227b
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2025-01-30 16:50:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (44.84%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,32 +1,17 @@
 class Solution {
-private:
-    int fun (vector<int> &nums, int n, vector<int> dp) {
-        // memoization
-        if (n == 0) return nums[n];
-        if (n < 0) return 0;
-        if (dp[n] != -1) return dp[n];
-        int a = nums[n] + fun(nums, n - 2, dp);
-        int b = fun(nums, n - 1, dp);
-        return dp[n] = max(a, b);
-    }
-
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-
-        // tabulation
         if (n == 1) return nums[0];
 
-        vector<int> dp(n, 0);
-
+        vector<int> dp(n + 1, 0);
         dp[0] = nums[0];
         dp[1] = max(nums[0], nums[1]);
 
         for (int i = 2; i < n; i++) {
-            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2]);
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
         }
-
+        
         return dp[n - 1];
-
     }
 };
