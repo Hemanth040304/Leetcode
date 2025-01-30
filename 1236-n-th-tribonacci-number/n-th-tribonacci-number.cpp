0db20df557010aea6ce4ba469c12fc06a607c3b5
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2025-01-30 16:41:52 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (46.90%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    int fun(int n, vector<int> &dp){
-        if(n < 2)return n;
-        if(n == 2)return 1;
-        if(dp[n] != -1)return dp[n];
-        return dp[n] = fun(n-1,dp)+fun(n-2,dp)+fun(n-3,dp);
-    }
     int tribonacci(int n) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(NULL);
-        cout.tie(NULL);
-        
-        vector<int> dp(n+1, -1);
-        return fun(n,dp);
+        vector<int> dp(38, 0);
+        dp[1] = 1;
+        dp[2] = 1;
+
+        if (n <= 2) return dp[n];
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        return dp[n];
     }
 };
