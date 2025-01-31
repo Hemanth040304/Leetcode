f4d8e1b9bb92dd4300b5d914b7c779852b24ed40
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2025-01-31 12:03:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (14.71%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -7,27 +7,20 @@ public:
         int left = 0, right = cols - 1, top = 0, bottom = rows - 1;
         
         while (left <= right and top <= bottom) {
-            for (int i = left; i <= right; i++) {
-                ans.emplace_back(matrix[top][i]);
-            }
+
+            for (int i = left; i <= right; i++) ans.emplace_back(matrix[top][i]);
             top++;
             
-            for (int i = top; i <= bottom; i++) {
-                ans.emplace_back(matrix[i][right]);
-            }
+            for (int i = top; i <= bottom; i++) ans.emplace_back(matrix[i][right]);
             right--;
             
             if (top <= bottom) {
-                for (int i = right; i >= left; i--) {
-                    ans.emplace_back(matrix[bottom][i]);
-                }
+                for (int i = right; i >= left; i--) ans.emplace_back(matrix[bottom][i]);
                 bottom--;
             }
             
             if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    ans.emplace_back(matrix[i][left]);
-                }
+                for (int i = bottom; i >= top; i--) ans.emplace_back(matrix[i][left]);
                 left++;
             }
         }
